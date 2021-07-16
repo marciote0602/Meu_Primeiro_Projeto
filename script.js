40b0f6de53d3e3c535e0de9de81bcf93194fee16
marciote0602 <marciote0602@gmail.com>
2021-07-15 22:33:14 -0300

    Meu Segundo Projeto

---
+++ script.js
@@ -0,0 +1,14 @@
+const pessoa = {nome: 'Márcio', idade:'44'};
+
+console.log(pessoa.nome);
+
+const quadrado = {
+    base:10, 
+    altura:20,
+    calculaArea: function(){return this.base*this.altura}
+};
+
+console.log(quadrado.calculaArea());
+
+
+
